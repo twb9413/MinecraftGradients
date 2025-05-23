b7764d05c2e55090fc2bde37f775af64f95d1ff6
twb9413 <tbrad9917@gmail.com>
2025-05-22 22:53:27 -0400

    basic files added

---
+++ imageProcessor.py
@@ -0,0 +1,12 @@
+"""
+Author: twb9413
+File: imageProcessor.py
+
+Desc:
+This file hanldes all functions image related
+"""
+
+
+class Image:
+    def __init__(self, file):
+        
---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+Author: twb9413
+File: main.py
+
+Desc: 
+The main file responsible for determining average color of a photo
+"""
+
+import imageProcessor
+
+def main():
+    imageProcessor.testFunc()
+    return 0
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
